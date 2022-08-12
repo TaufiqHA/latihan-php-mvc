0818d4ca75c7dd2995543880de1512ca92d7ae33
TaufiqHA <htaufiq225@gmail.com>
2022-08-12 11:02:28 +0700

    update file app.php

--- app/Core/App.php
+++ app/Core/App.php
@@ -11,7 +11,9 @@ class App
 	public function parsURL()
 	{
 		if (isset($_GET['url'])) {
-			$url = $_GET['url'];
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
 			return $url;
 		}
 	}
