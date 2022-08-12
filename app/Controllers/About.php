8cd5843f2fb379a3b34564260f61b80d6059630a
TaufiqHA <htaufiq225@gmail.com>
2022-08-12 14:41:53 +0700

    mengubah file about.php

--- app/Controllers/About.php
+++ app/Controllers/About.php
@@ -2,9 +2,9 @@
 
 class About
 {
-	public function index(string $nama = "Taufiq", string $pekerjaan = "Programmer") 
+	public function index(string $nama = "Taufiq", string $pekerjaan = "Programmer", int $umur = 20) 
 	{
-		echo "Halo nama saya $nama, pekerjaan saya $pekerjaan";
+		echo "Halo nama saya $nama, pekerjaan saya $pekerjaan. saya berumur $umur tahun";
 	}
 	
 	public function page()
