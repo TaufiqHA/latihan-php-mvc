f2ad2cea049aa16b0de5274b1d26e77d6815c4b7
TaufiqHA <htaufiq225@gmail.com>
2022-08-12 09:05:58 +0700

    update ke 2

--- app/Core/App.php
+++ app/Core/App.php
@@ -2,5 +2,17 @@
 
 class App 
 {
-	
+	public function __construct()
+	{
+		$url = $this->parsURL();
+		var_dump($url);
+	}
+
+	public function parsURL()
+	{
+		if (isset($_GET['url'])) {
+			$url = $_GET['url'];
+			return $url;
+		}
+	}
 }
