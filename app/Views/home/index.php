efa83d0225f957c8ea8366f034c028cee34bc859
TaufiqHA <htaufiq225@gmail.com>
2022-08-13 09:28:26 +0700

    menambahkan style baru pada halaman home/index.php

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,11 +1,14 @@
 <section class="pt-32">
 
-    <div class="container mx-auto bg-teal-500 px-8 py-10">
+    <div class="container mx-auto px-8 py-10">
         <div class="flex flex-wrap">
-            <div class="">
-            <h1 class="font-semibold text-white "> Haloo...👋</h1>
-            <h2 class="font-bold text-4xl text-slate-800 mt-2"> Selamat Datang </h2>
-            <h3 class="font-bold text-lg text-slate-600 mt-2"> Di Website Saya </h3>
+            <div class="max-w-lg">
+                <h1 class="font-semibold text-teal-500 "> Haloo...👋</h1>
+                <h2 class="font-bold text-4xl text-slate-800 mt-2"> Selamat Datang </h2>
+                <h3 class="font-bold text-lg text-slate-600 mt-2"> Di Website Saya </h3>
+            </div>
+            <div class="hero">
+                
             </div>
         </div>
     </div>
