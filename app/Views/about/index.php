d4219d37b27b4edf902fd6ded88b6534588823f8
TaufiqHA <htaufiq225@gmail.com>
2022-08-13 10:39:05 +0700

    menambahkan style pada file about/index.php

--- app/Views/about/index.php
+++ app/Views/about/index.php
@@ -1,2 +1,10 @@
-<h2> Halaman About </h2>
-<p> Halo nama saya <?php echo $data['nama'] ?>, umur saya <?php echo $data['umur'] ?>. Saya adalah seorang <?php echo $data['pekerjaan'] ?></p>
+<section class="pt-32 pb-20">
+    <div class="container mx-auto">
+        <div class="flex flex-wrap">
+             <div>
+                <h2> Halaman About </h2>
+                <p> Halo nama saya <?php echo $data['nama'] ?>, umur saya <?php echo $data['umur'] ?>. Saya adalah seorang <?php echo $data['pekerjaan'] ?></p>
+            </div>
+        </div>
+    </div>
+</section>
