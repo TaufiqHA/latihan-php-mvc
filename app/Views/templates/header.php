da00b6831f98b4df1db5b406e34b53988ac6f211
TaufiqHA <htaufiq225@gmail.com>
2022-08-13 10:19:55 +0700

    update navbar

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -12,7 +12,7 @@
         <div class="container">
             <div class="flex items-center justify-between relative ">
                 <div class="px-4">
-                    <a href="#home" class="font-bold text-lg text-primary block py-6 ">Taufiq Hidayah A.</a>
+                    <a href="<?php echo BASEURL; ?>" class="font-bold text-lg text-primary block py-6 ">Taufiq Hidayah A.</a>
                 </div>
                 <div class="flex items-center px-4 ">
                     <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden">
@@ -30,16 +30,16 @@
                                 <a href="<?php echo BASEURL; ?>/about" class="text-base py-2 mx-8 flex  group-hover:text-primary">About</a>
                             </li>
                             <li class="group">
-                                <a href="#portfolio" class="text-base py-2 mx-8 flex  group-hover:text-primary">Portfolio</a>
+                                <a href="#" class="text-base py-2 mx-8 flex  group-hover:text-primary">Portfolio</a>
                             </li>
                             <li class="group">
-                                <a href="#client" class="text-base py-2 mx-8 flex  group-hover:text-primary">Clients</a>
+                                <a href="#" class="text-base py-2 mx-8 flex  group-hover:text-primary">Clients</a>
                             </li>
                             <li class="group">
-                                <a href="#blog" class="text-base py-2 mx-8 flex  group-hover:text-primary">Blog</a>
+                                <a href="#" class="text-base py-2 mx-8 flex  group-hover:text-primary">Blog</a>
                             </li>
                             <li class="group">
-                                <a href="#contact" class="text-base py-2 mx-8 flex  group-hover:text-primary">Contact</a>
+                                <a href="#" class="text-base py-2 mx-8 flex  group-hover:text-primary">Contact</a>
                             </li>
                         </ul>
                     </nav>
