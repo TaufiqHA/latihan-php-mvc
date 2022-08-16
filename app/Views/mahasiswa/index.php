fb7600e31b17124e9dceae895914b9babb58f3bd
TaufiqHA <htaufiq225@gmail.com>
2022-08-16 21:41:00 +0700

    menghapus fitur updata

--- app/Views/mahasiswa/index.php
+++ app/Views/mahasiswa/index.php
@@ -6,12 +6,12 @@
                 <button class="font-semibold p-2 px-10 bg-teal-500 mb-5 rounded-xl text-white" id="button2_modal"> Tambah Mahasiswa </button>
                 <ul>
                     <?php foreach ($data['mhs'] as $mhs) : ?>
-                        <li class="font-semibold border border-slate-500 px-5 py-2 w-[600px] flex justify-between ">
+                        <li class="font-semibold border border-slate-500 px-5 py-2 w-[600px]">
                             <?php echo $mhs['nama'] ?>
-                            <span class="flex justify-evenly w-1/2">
-                                <a href="<?php echo BASEURL; ?>/Mahasiswa/detail/<?php echo $mhs['id'] ?>" class="text-white bg-sky-600 px-3 rounded-full">detail</a>
-                                <a href="<?php echo BASEURL; ?>/Mahasiswa/hapus/<?php echo $mhs['id'] ?>" class="text-white bg-pink-600 px-3 rounded-full">hapus</a>
-                                <a href="<?php echo BASEURL; ?>/Mahasiswa/update/<?php echo $mhs['id'] ?>" class="text-white bg-yellow-600 px-3 rounded-full" id="button_update">update</a>
+                            <span class="inline-block w-1/2 float-right">
+                                <a href="<?php echo BASEURL; ?>/Mahasiswa/hapus/<?php echo $mhs['id'] ?>" class="text-white bg-pink-600 px-3 rounded-full float-right">hapus</a>
+                                <a href="<?php echo BASEURL; ?>/Mahasiswa/ubah/<?php echo $mhs['id'] ?>" class="text-white bg-teal-600 px-3 rounded-full float-right">ubah</a>
+                                <a href="<?php echo BASEURL; ?>/Mahasiswa/detail/<?php echo $mhs['id'] ?>" class="text-white bg-sky-600 px-3 rounded-full float-right">detail</a>
                             </span>
                         </li>
                     <?php endforeach; ?>
@@ -36,26 +36,6 @@
             </div>
             <div class="button w-full flex justify-end">
                 <button class="p-1 bg-teal-500 rounded-md px-3 font-semibold text-md text-white" form="modal" type="submit"> Tambah </button>
-            </div>
-        </div>
-
-        <!-- update modal box -->
-
-        <div class="modal-box w-1/3 bg-white border border-slate-600 rounded-xl py-5 px-10 absolute right-1/2 top-10 translate-x-1/2 shadow-2xl z-50 hidden_modal" id="update_modal_box">
-            <div class="judul-modal font-semibold text-lg mb-7 flex justify-between">
-                <h1> Update Data Mahasiswa </h1>
-                <a href="#" class="hover:cursor-pointer" id="button_modal_update"><img src="<?php echo BASEURL; ?>/img/close.svg" alt="close"></a>
-            </div>
-            <div class="form-modal">
-                <form action="<?php echo BASEURL; ?>/mahasiswa/index/<?php echo $mhs['id'] ?>" id="update" method="post">
-                    <label for="name" class="font-semibold mb-3 block"> Nama </label>
-                    <input type="text" class="mb-7 p-2 w-full rounded-lg outline-none border border-teal-500 focus:ring focus:ring-teal-500" placeholder="..." name="name" value="Taufiq">
-                    <label for="nim" class="font-semibold mb-3 block"> NIM </label>
-                    <input type="text" class="mb-7 p-2 w-full rounded-lg outline-none border border-teal-500 focus:ring focus:ring-teal-500" placeholder="..." name="nim" value="60600120046">
-                </form>
-            </div>
-            <div class="button w-full flex justify-end">
-                <button class="p-1 bg-teal-500 rounded-md px-3 font-semibold text-md text-white" form="update" type="submit"> Update </button>
             </div>
         </div>
     </div>
